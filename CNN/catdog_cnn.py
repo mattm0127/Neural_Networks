fe77519c22a4aa451ba412f6d8bf966d7d23e11a
Matt Mennella <matt.mennella@gmail.com>
2026-04-24 08:05:32 -0400

    added comments and some more type annotations

--- CNN/catdog_cnn.py
+++ CNN/catdog_cnn.py
@@ -1,48 +1,51 @@
+import kagglehub
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 from torchvision.transforms import v2
-import kagglehub
 
+# Path to dataset for training
 path = kagglehub.dataset_download("tongpython/cat-and-dog")
 
+# Detect which device to perform training
 if torch.cuda.is_available():
-    device = 'cuda'
+    device = "cuda"
 elif torch.xpu.is_available():
-    device = 'xpu'
+    device = "xpu"
 else:
-    device = 'cpu'
+    device = "cpu"
+
 
 class CatDogCNN(nn.Module):
-
     def __init__(self):
         super().__init__()
 
-        self.gpu_train_transform = v2.Compose([
-            # Spacial Variations
-            v2.RandomResizedCrop(128, scale=(0.5, 1.0)),
-            v2.RandomHorizontalFlip(p=0.5),
-            v2.RandomRotation(25),
-            #  appearance variations
-            v2.ColorJitter(
-                brightness=0.3,
-                contrast=0.3,
-                saturation=0.3,
-                hue=0.1
-            ),
-            v2.ToDtype(torch.float32, scale=True),
-            v2.RandomErasing(p=0.2),
-            v2.Normalize((0.5, 0.5, 0.5),
-                                (0.5, 0.5, 0.5))
-        ])
+        # Image training transformations
+        self.gpu_train_transform = v2.Compose(
+            [
+                # Spacial Variations
+                v2.RandomResizedCrop(128, scale=(0.5, 1.0)),
+                v2.RandomHorizontalFlip(p=0.5),
+                v2.RandomRotation(25),
+                #  appearance variations
+                v2.ColorJitter(brightness=0.3, contrast=0.3, saturation=0.3, hue=0.1),
+                v2.ToDtype(torch.float32, scale=True),
+                v2.RandomErasing(p=0.2),
+                v2.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            ]
+        )
 
-        self.gpu_test_transform = v2.Compose([
-            v2.ToDtype(torch.float32, scale=True),
-            v2.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-        ])
+        # Image testing transformations
+        self.gpu_test_transform = v2.Compose(
+            [
+                v2.ToDtype(torch.float32, scale=True),
+                v2.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            ]
+        )
 
+        # Feature Extraction
         self.features = nn.Sequential(
             # Block 1 - 128x128 -> 64x64
             nn.Conv2d(3, 32, kernel_size=3, padding=1),
@@ -59,18 +62,19 @@ class CatDogCNN(nn.Module):
             nn.BatchNorm2d(128),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),
-            #Block 4 - 16x16 -> 8x8
+            # Block 4 - 16x16 -> 8x8
             nn.Conv2d(128, 256, kernel_size=3, padding=1),
             nn.BatchNorm2d(256),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),
-            #Block 5 - 8x8 -> 4x4
+            # Block 5 - 8x8 -> 4x4
             nn.Conv2d(256, 512, kernel_size=3, padding=1),
             nn.BatchNorm2d(512),
             nn.ReLU(),
-            nn.MaxPool2d(2, 2)
+            nn.MaxPool2d(2, 2),
         )
-        
+
+        # Classifier
         self.classifier = nn.Sequential(
             nn.AdaptiveAvgPool2d(1),
             nn.Flatten(),
@@ -80,10 +84,10 @@ class CatDogCNN(nn.Module):
             nn.Linear(512, 128),
             nn.ReLU(),
             nn.Dropout(0.5),
-            nn.Linear(128, 2)
+            nn.Linear(128, 2),
         )
-    
-    def forward(self, x):
+
+    def forward(self, x: torch.Tensor):
         if self.training:
             x = self.gpu_train_transform(x)
         else:
@@ -91,38 +95,43 @@ class CatDogCNN(nn.Module):
 
         x = self.features(x)
         x = self.classifier(x)
-        
+
         return x
 
 
-def get_data(root_path):
-    
-    cpu_transform = v2.Compose([
-        v2.ToImage(),
-        v2.ToDtype(torch.uint8, scale=True),
-    ])
+def get_data(root_path: str):
+    """Prepare data for training and testing
 
-    transform_test = transforms.Compose([
-        transforms.ToTensor(),
-    ])
+    Args:
+        root_path (str): Path to the training/testing data
+
+    Returns:
+        (DataLoader, DataLoader): Training and Testing DataLoaders
+    """
+
+    # Transformations prior to GPU transformations
+    cpu_transform = v2.Compose(
+        [
+            v2.ToImage(),
+            v2.ToDtype(torch.uint8, scale=True),
+        ]
+    )
 
     train_dataset = datasets.ImageFolder(
-        root=f"{root_path}/training_set/training_set",
-        transform=cpu_transform
-    )                     
+        root=f"{root_path}/training_set/training_set", transform=cpu_transform
+    )
 
     test_dataset = datasets.ImageFolder(
-        root=f"{root_path}/test_set/test_set",
-        transform=cpu_transform
+        root=f"{root_path}/test_set/test_set", transform=cpu_transform
     )
 
     train_loader = DataLoader(
-        train_dataset, 
-        batch_size=128, 
+        train_dataset,
+        batch_size=128,
         shuffle=True,
         num_workers=2,
         persistent_workers=True,
-        pin_memory=True
+        pin_memory=True,
     )
 
     test_loader = DataLoader(
@@ -131,7 +140,7 @@ def get_data(root_path):
         shuffle=False,
         num_workers=2,
         persistent_workers=True,
-        pin_memory=True
+        pin_memory=True,
     )
 
     print(f"Classes: {train_dataset.classes}")
@@ -140,12 +149,24 @@ def get_data(root_path):
 
     return train_loader, test_loader
 
-def train(model, train_loader, test_loader, criterion, optimizer, scheduler, epochs=1):
+
+def train(
+    model: nn.Module,
+    train_loader: DataLoader[tuple[torch.Tensor, torch.Tensor]],
+    test_loader: DataLoader,
+    criterion: nn.Module,
+    optimizer: optim.Optimizer,
+    scheduler,
+    epochs=1,
+):
     top_accuracy = 0
     for epoch in range(epochs):
         model.train()
         for data, target in train_loader:
-            data, target = data.to(device, non_blocking=True), target.to(device, non_blocking=True)
+            data, target = (
+                data.to(device, non_blocking=True),
+                target.to(device, non_blocking=True),
+            )
             optimizer.zero_grad()
             output = model(data)
             loss = criterion(output, target)
@@ -157,11 +178,13 @@ def train(model, train_loader, test_loader, criterion, optimizer, scheduler, epo
 
         if accuracy > top_accuracy:
             top_accuracy = accuracy
-            torch.save(model.state_dict(), 'catdog.pth')
+            torch.save(model.state_dict(), "catdog.pth")
 
         if (epoch + 1) % 5 == 0:
-            print(f"Epoch {epoch + 1} | Loss: {loss.item()} | Accuracy: {accuracy:2f} | Learning Rate: {optimizer.param_groups[0]['lr']}")
-            
+            print(
+                f"Epoch {epoch + 1} | Loss: {loss.item()} | Accuracy: {accuracy:2f} | Learning Rate: {optimizer.param_groups[0]['lr']}"
+            )
+
 
 def test(model, test_loader):
     model.eval()
@@ -183,8 +206,7 @@ if __name__ == "__main__":
 
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=0.001, weight_decay=1e-4)
-    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='max', patience=5, factor=0.5)
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(
+        optimizer, mode="max", patience=5, factor=0.5
+    )
     train(model, train_loader, test_loader, criterion, optimizer, scheduler, epochs=50)
-
-
-
--- CNN/model_test.py
+++ CNN/model_test.py
@@ -3,57 +3,62 @@ import torch
 from torchvision.transforms import v2
 from PIL import Image, ImageOps
 import matplotlib.pyplot as plt
-
+import math
 
 if torch.cuda.is_available():
-    device = 'cuda'
+    device = "cuda"
 elif torch.xpu.is_available():
-    device = 'xpu'
+    device = "xpu"
 else:
-    device = 'cpu'
+    device = "cpu"
 
-inference_transform = v2.Compose([
-    v2.ToImage(),
-    v2.ToDtype(torch.uint8, scale=True),
-    v2.Resize((128, 128)),
-])
+inference_transform = v2.Compose(
+    [
+        v2.ToImage(),
+        v2.ToDtype(torch.uint8, scale=True),
+        v2.Resize((128, 128)),
+    ]
+)
 
 model = CatDogCNN().to(device)
 
-model.load_state_dict(torch.load(
-    r"C:\Users\mattm\OneDrive\Desktop\PyTorch Models\CNN\catdog_93.pth",
-    map_location=device, 
-    weights_only=True
-))
+model.load_state_dict(
+    torch.load(
+        r"C:\Users\mattm\OneDrive\Desktop\PyTorch Models\CNN\catdog_93.pth",
+        map_location=device,
+        weights_only=True,
+    )
+)
 model.eval()
+
 
 def predict(model, img_path):
     img = Image.open(img_path).convert("RGB")
     img_tensor = inference_transform(img)
     img_tensor = img_tensor.unsqueeze(0).to(device)
 
-
-    with torch.no_grad(): # Don't track gradients (saves memory)
+    with torch.no_grad():  # Don't track gradients (saves memory)
         output: torch.Tensor = model(img_tensor)
         probabilities = torch.nn.functional.softmax(output, dim=1)
         confidence, predicted = torch.max(probabilities, 1)
 
-    if confidence < .85:
-        result = 'No Animal Detected'
+    if confidence < 0.85:
+        result = "No Animal Detected"
     else:
-        classes = ['cat', 'dog']
+        classes = ["cat", "dog"]
         result = classes[predicted.item()]
-    print(f"Prediction: {result} | Confidence: {confidence.item()*100:.2f}")
+    print(f"Prediction: {result} | Confidence: {confidence.item() * 100:.2f}")
 
 
 def display_layer(model, img_path, layer=0):
 
     activations = {}
+
     def hook_fn(model, input, output):
         # This grabs the output of the layer as it flies by
-        activations['features'] = output.detach()
+        activations["features"] = output.detach()
 
-    target_layer = model.features[layer] 
+    target_layer = model.features[layer]
     target_layer.register_forward_hook(hook_fn)
 
     img = Image.open(img_path).convert("RGB")
@@ -65,13 +70,17 @@ def display_layer(model, img_path, layer=0):
         # Pass in a single image from your test set
         _ = model(img_tensor)
 
-    act = activations['features'].squeeze()
-    fig, axes = plt.subplots(15, 18, figsize=(10, 10))
+    act = activations["features"].squeeze()
+    grid = int(math.sqrt(act.shape[0] / 2))
+    fig, axes = plt.subplots(grid, grid, figsize=(10, 10))
     for i, ax in enumerate(axes.flat):
         if i < act.shape[0]:
-            ax.imshow(act[i].cpu().numpy(), cmap="Grays")
-            ax.axis('off')
+            ax.imshow(act[i].cpu().numpy(), cmap="gist_gray")
+            ax.axis("off")
     plt.suptitle(f"What the Model Sees At Layer {layer}")
     plt.show()
 
-display_layer(model, r"C:\Users\mattm\Downloads\PXL_20211030_022346138.PORTRAIT.jpg", 16)
+
+display_layer(
+    model, r"C:\Users\mattm\Downloads\PXL_20250714_113507485.jpg", 0
+)
